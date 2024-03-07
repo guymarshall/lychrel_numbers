5600f6b9781e67e35ea2fc1534d7a82ad7726dd0
Guy Marshall <guymarshall9@gmail.com>
2024-03-07 12:34:15 +0000

    UPDATE: Use BigInt for larger numbers

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ const LARGE_TEST: u128 = 89;
 const VERY_LARGE_TEST: u128 = 1_186_060_307_891_929_990;
 const POTENTIALLY_INFINITE: u128 = 196;
 
-fn is_palindrome(number: u32) -> bool {
-    number.to_string().chars().rev().collect::<String>() == number.to_string()
+use num::{BigInt, One};
+
+fn is_palindrome(number: BigInt) -> bool {
+    let number_string: String = number.to_str_radix(10);
+    let reversed_string: String = number_string.chars().rev().collect();
+    number_string == reversed_string
 }
 
-fn add_inverted(number: u32) -> u32 {
-    let reversed_string: String = number.to_string().chars().rev().collect();
-    number + reversed_string.parse::<u32>().unwrap_or(0)
+fn add_inverted(number: BigInt) -> BigInt {
+    number.clone() + BigInt::parse_bytes(number.to_str_radix(10).chars().rev().collect::<String>().as_bytes(), 10).unwrap_or(BigInt::one())
 }
 
 fn main() {
-    let original_number: u32 = 254;
-    let mut number: u32 = original_number;
+    let original_number: BigInt = 89.into();
+    let mut number: BigInt = original_number.clone();
     let mut steps: u32 = 0;
 
-    while !is_palindrome(number) {
-        number = add_inverted(number);
+    while !is_palindrome(number.clone()) {
+        number = add_inverted(number.clone());
         steps += 1;
     }
 
