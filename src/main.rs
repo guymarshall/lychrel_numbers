91a7c2b98d82c0b2c57353573873b3476fff1bf8
Guy Marshall <guymarshall9@gmail.com>
2024-03-14 19:35:28 +0000

    UPDATE: Pass in number by reference not cloned value

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,23 @@ const POTENTIALLY_INFINITE: u128 = 196;
 
 use num::{BigInt, One};
 
-fn is_palindrome(number: BigInt) -> bool {
+fn is_palindrome(number: &BigInt) -> bool {
     let number_string: String = number.to_str_radix(10);
     let reversed_string: String = number_string.chars().rev().collect();
     number_string == reversed_string
 }
 
-fn add_inverted(number: BigInt) -> BigInt {
+fn add_inverted(number: &BigInt) -> BigInt {
     number.clone() + BigInt::parse_bytes(number.to_str_radix(10).chars().rev().collect::<String>().as_bytes(), 10).unwrap_or(BigInt::one())
 }
 
 fn main() {
-    let original_number: BigInt = VERY_LARGE_TEST.into();
+    let original_number: BigInt = POTENTIALLY_INFINITE.into();
     let mut number: BigInt = original_number.clone();
     let mut steps: u32 = 0;
 
-    while !is_palindrome(number.clone()) {
-        number = add_inverted(number.clone());
+    while !is_palindrome(&number) {
+        number = add_inverted(&number);
         steps += 1;
     }
 
